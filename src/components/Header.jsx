b46cccc2490132f02b97765c2cfbbfda23ec2c95
Muhammad Delowar Hosen MI <md48735002@gmail.com>
2024-12-30 11:42:43 +0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
     <div className='bg-subnavColor py-5  '>
       <Container>
       <Flex className={' mobile:gap-x-0 android:gap-x-1 taplet:gap-x-2 laptop:gap-x-3'}>
-        <div className="pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
+        <div className="w-[20%] pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
         <Category/>
         </div>
-        <div className="w-full relative mobile:w-[300px] android:w-[550px] taplet:w-[700px] laptop:w-[1200px]">
-          <input type="text"className='className = p-3 bg-white rounded-lg w-full border border-InfoColor'placeholder='Search Products' />
+        <div className="w-[60%] relative mobile:w-[300px] android:w-[550px] taplet:w-[700px] laptop:w-[1200px]">
+          <input type="text"className=' p-3 bg-white rounded-lg w-full border border-InfoColor'placeholder='Search Products' />
           <FaSearch className='absolute right-4 top-1/2 -translate-y-1/2'/>
         </div>
-        <div className=" pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
+        <div className="w-[20%] pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
         <Flex className={'justify-center'}>
                 <User/>
                 <FaSortDown className='ml-1'/>
